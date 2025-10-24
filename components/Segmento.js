73c7cc0f9ee65366e33027ee8ee5a4f96be77a00
ataca3000 <117702185+ataca3000@users.noreply.github.com>
2025-10-23 20:55:06 -0600

    Update Segmento.js

--- components/Segmento.js
+++ components/Segmento.js
@@ -1,8 +1,7 @@
+
 export default function Segmento({ titulo, imagen, onClick }) {
   return (
-    <div 
-      className="bg-gray-950 rounded-lg shadow-lg overflow-hidden flex flex-col hover:scale-105 transition-transform duration-300"
-    >
+    <div className="bg-gray-950 rounded-lg shadow-lg overflow-hidden flex flex-col hover:scale-105 transition-transform duration-300">
       <img src={imagen} alt={titulo} className="w-full h-56 object-cover" />
       <div className="p-4 flex flex-col flex-grow justify-between text-center">
         <h3 className="text-xl font-bold mb-4">{titulo}</h3>
