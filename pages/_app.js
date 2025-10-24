0fb984bea6eed39942208c8b58586b1c8475991d
ataca3000 <117702185+ataca3000@users.noreply.github.com>
2025-10-23 19:45:57 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,10 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function App({ componets, pageProps }) {
+export default function App({ components, pageProps }) {
   return (
-    <main className={inter.className}>
-      <componets {...pageProps} />
+    <main className={inter.className}>t
+      <components {...pageProps} />
     </main>
   )
 }
