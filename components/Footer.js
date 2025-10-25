10b3a98948fbab970878c28bae99c338543f9e41
ataca3000 <117702185+ataca3000@users.noreply.github.com>
2025-10-25 09:50:01 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,42 +1,18 @@
-import React from 'react'
-
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-400 py-8 mt-12">
-      <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-6">
-        
-        {/* Logo o nombre */}
-        <div className="flex items-center gap-3">
-          <img src="/img/logo.png" alt="Logo" className="h-10 w-auto" />
-          <span className="text-orange-500 font-bold text-lg">
-            Multiservicios e Iluminación Veracruz
-          </span>
-        </div>
-
-        {/* Navegación rápida */}
-        <div className="flex gap-6">
-          <a href="#" className="hover:text-orange-500 transition">Inicio</a>
-          <a href="#" className="hover:text-orange-500 transition">Servicios</a>
-          <a href="#" className="hover:text-orange-500 transition">Contacto</a>
-        </div>
-
-        {/* Redes sociales */}
-        <div className="flex gap-4">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-orange-500 transition">
-            Facebook
-          </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-orange-500 transition">
-            Instagram
-          </a>
-          <a href="mailto:contacto@empresa.com" className="hover:text-orange-500 transition">
-            Email
-          </a>
-        </div>
+    <footer className='bg-black text-white py-6 flex flex-col items-center gap-4'>
+      <div className='flex gap-6'>
+        <a href='https://facebook.com' target='_blank' rel='noopener noreferrer'>
+          <img src='/icons/facebook.svg' alt='Facebook' className='w-6 h-6' />
+        </a>
+        <a href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
+          <img src='/icons/instagram.svg' alt='Instagram' className='w-6 h-6' />
+        </a>
+        <a href='https://maps.google.com/?q=Multiservicios+Veracruz' target='_blank' rel='noopener noreferrer'>
+          <img src='/icons/location.svg' alt='Ubicación' className='w-6 h-6' />
+        </a>
       </div>
-
-      <div className="text-center text-sm text-gray-500 mt-6">
-        © {new Date().getFullYear()} Multiservicios e Iluminación Veracruz. Todos los derechos reservados.
-      </div>
+      <p className='text-sm text-gray-400'>© 2025 Multiservicios e Iluminación Veracruz</p>
     </footer>
   )
 }
