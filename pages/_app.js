b6e0421721881ae38b1d4b9451106b30efb19e49
ataca3000 <117702185+ataca3000@users.noreply.github.com>
2025-10-28 22:24:50 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+// pages/_app.js
+import { CartProvider } from '../context/CartContext'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  )
 }
