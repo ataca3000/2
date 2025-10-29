75ac438910947831f621fdf961c6a1316d196a86
ataca3000 <117702185+ataca3000@users.noreply.github.com>
2025-10-28 22:50:14 -0600

    Update CatalogoLed.js

--- components/CatalogoLed.js
+++ components/CatalogoLed.js
@@ -32,9 +32,7 @@ export default function CatalogoLed() {
       </h2>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
         {products.map((product) => (
-          <div key={product.id}>
-          <div className='relative bg-gray-800 rounded-lg overflow-hidden shadow-lg'
-          
+          <div key={product.id} className='relative bg-gray-800 rounded-lg overflow-hidden shadow-lg'>
             <Image
               src={product.image}
               alt={product.title}
@@ -79,5 +77,3 @@ export default function CatalogoLed() {
     </section>
   )
 }
-  )
-                    }
