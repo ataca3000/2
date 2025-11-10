2eaec4642ecbd9e695526dc641c803f66e1e3057
ataca3000 <117702185+ataca3000@users.noreply.github.com>
2025-11-10 09:36:31 -0600

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: https: '2-g664-1l96fovoa-felipes-projects-e1164021.vercel.app'],
-  },
-}
-
-module.exports = nextConfig
